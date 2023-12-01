0629cb63087f8f533bc448233d496d01e5a29e15
korny-ffm <97454447+korny-ffm@users.noreply.github.com>
2023-12-01 11:22:03 +0100

    Update Klasse.java

--- test5/src/test5/Klasse.java
+++ test5/src/test5/Klasse.java
@@ -5,7 +5,7 @@ public class Klasse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("fddfd");
+		System.out.println("Richtiger Text");
 		
 	}
 
